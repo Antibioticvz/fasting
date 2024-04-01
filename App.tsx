a936f4c444c5f05439fe6723685d6ef0ceca0ec8
Victor Zadorozhnyy <antibioticvz@gmail.com>
2024-04-01 22:01:01 +0300

    fix settings

--- App.tsx
+++ App.tsx
@@ -8,8 +8,8 @@ const StyledText = styled(Text)
 
 export default function App() {
   return (
-    <StyledView className="flex-1 items-center justify-center bg-green">
-      <StyledText tw="font-bold">
+    <StyledView className="flex-1 items-left justify-center bg-amber-400">
+      <StyledText tw="font-bold italic">
         Open up App.js to start working on your app!!
       </StyledText>
       <StatusBar style="auto" />
